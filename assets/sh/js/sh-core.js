bd9d70d9468f1ed1c13cb6c2f989f12eff916e4b
David <77289240+davidmadmin@users.noreply.github.com>
2025-11-04 14:47:59 +0100

    Delete assets directory

--- assets/sh/js/sh-core.js
+++
@@ -1,3 +0,0 @@
-// SH Core Utilities
-// Reserved for shared helpers between SH feature modules. Currently no global helpers are required,
-// but keep this file registered first so future shared utilities can be added without reordering other bundles.
