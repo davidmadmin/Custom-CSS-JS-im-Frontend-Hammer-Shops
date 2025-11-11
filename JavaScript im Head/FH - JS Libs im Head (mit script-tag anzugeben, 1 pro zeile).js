1cf0c3c8f7b1fdf903d66b07ccaf2d675740d293
David <77289240+davidmadmin@users.noreply.github.com>
2025-11-11 12:12:53 +0100

    Update FH - JS Libs im Head (mit script-tag anzugeben, 1 pro zeile).js

--- JavaScript im Head/FH - JS Libs im Head (mit script-tag anzugeben, 1 pro zeile).js
+++ JavaScript im Head/FH - JS Libs im Head (mit script-tag anzugeben, 1 pro zeile).js
@@ -1,2 +1,5 @@
 <script src="https://integrations.etrusted.com/applications/widget.js/v2" defer async></script>
+<script src="https://cdn.lottielab.com/s/lottie-player@1.x/player-web.min.js"></script>
+<script src="https://cdn02.plentymarkets.com/nteqnk1xxnkn/frontend/Custom_JS_Code/FH_Custom_JS_Code/FH_-_Javascript_am_Ende_der_Seite_pr408.js" async></script>
+<script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.6.2/dist/dotlottie-wc.js" type="module"></script>
 <script src="https://eu1-config.doofinder.com/2.x/915effe0-ee43-4db2-9567-5ee87a7bc008.js" async></script>
