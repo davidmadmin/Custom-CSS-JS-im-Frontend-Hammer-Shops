3f977d3f795e411475203031e0c61d20be325a4c
David <77289240+davidmadmin@users.noreply.github.com>
2026-02-04 15:11:44 +0100

    feat(slider): add quick navigation menu

--- JavaScript im Head/FH - JS im Head.js
+++ JavaScript im Head/FH - JS im Head.js
@@ -3958,6 +3958,22 @@ fhOnReady(function () {
       return;
     }
 
+    var sliderWrapper = slider.closest('.fh-custom-slider');
+    var navLinks = sliderWrapper
+      ? sliderWrapper.querySelectorAll('.fh-custom-slider__nav-link')
+      : [];
+
+    var setActiveNav = function (index) {
+      if (!navLinks.length) {
+        return;
+      }
+      navLinks.forEach(function (link) {
+        var dataIndex = Number(link.getAttribute('data-slide-index'));
+        var isMatch = Number.isNaN(dataIndex) ? false : dataIndex === index;
+        link.classList.toggle('is-active', isMatch);
+      });
+    };
+
     var activeOverlay = null;
     var activateOverlay = function (index) {
       var nextOverlay = null;
@@ -3981,6 +3997,8 @@ fhOnReady(function () {
         nextOverlay.classList.add('is-active');
         activeOverlay = nextOverlay;
       }
+
+      setActiveNav(index);
     };
 
     var beginOverlayExit = function () {
